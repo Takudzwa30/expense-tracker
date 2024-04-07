fa45ac1e7d2068bcb66ea462fdef87323db96d4f
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-04-07 23:07:41 +0400

    added axios

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,16 +1,22 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import { GlobalStyles } from "../../constants/styles";
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-function Input({ label, textInputConfig, style }) {
+import { GlobalStyles } from '../../constants/styles';
+
+function Input({ label, invalid, style, textInputConfig }) {
+
   const inputStyles = [styles.input];
 
   if (textInputConfig && textInputConfig.multiline) {
-    inputStyles.push(styles.inputMultiLine);
+    inputStyles.push(styles.inputMultiline)
+  }
+
+  if (invalid) {
+    inputStyles.push(styles.invalidInput);
   }
 
   return (
     <View style={[styles.inputContainer, style]}>
-      <Text style={styles.label}>{label}</Text>
+      <Text style={[styles.label, invalid && styles.invalidLabel]}>{label}</Text>
       <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
@@ -21,7 +27,7 @@ export default Input;
 const styles = StyleSheet.create({
   inputContainer: {
     marginHorizontal: 4,
-    marginVertical: 8,
+    marginVertical: 8
   },
   label: {
     fontSize: 12,
@@ -30,13 +36,19 @@ const styles = StyleSheet.create({
   },
   input: {
     backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
     padding: 6,
     borderRadius: 6,
     fontSize: 18,
-    color: GlobalStyles.colors.primary700,
   },
-  inputMultiLine: {
+  inputMultiline: {
     minHeight: 100,
-    textAlignVertical: "top",
+    textAlignVertical: 'top'
   },
+  invalidLabel: {
+    color: GlobalStyles.colors.error500
+  },
+  invalidInput: {
+    backgroundColor: GlobalStyles.colors.error50
+  }
 });
